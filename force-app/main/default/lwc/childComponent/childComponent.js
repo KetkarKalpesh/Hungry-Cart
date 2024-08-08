b3f204b1436014082a23dce41832092f37b2e022
Kalpesh Ketkar <kalpeshketkar1001@gmail.com>
2024-08-08 23:04:38 +0530

    Updated Code

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,16 +1,25 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
-    @api childInputValue;
+    @api inputValue = '';
 
     handleChildInputChange(event) {
-        this.childInputValue = event.target.value;
+        this.inputValue = event.target.value;
     }
 
     handleSubmitToParent() {
-        const inputChangeEvent = new new CustomEvent('childChange', {
-            detail: {value: this.childInputValue}
+        const inputChangeEvent = new CustomEvent('childchange', {
+            detail: { value: this.inputValue }
         });
         this.dispatchEvent(inputChangeEvent);
+    }
+
+    @api
+    set inputvalue(value) {
+        this.inputValue = value;
+    }
+
+    get inputvalue() {
+        return this.inputValue;
     }
 }
